5dd6dc07ccc76d6fa7fb5b24d2f2af1d2daee6fd
Rahul Badgujar <44890430+imRB7here@users.noreply.github.com>
2021-01-04 21:18:40 +0530

    setted up Google Sign In testing button
    a button is added to MainActivity to test Google Sign In feature.
    activity_main.xml and MainActivity is modified.
    before this commit-> textview was present on MainActivity
    after this commit-> Continue with Google button is added in MainActivity

--- app/src/main/java/com/example/myeventsapp/MainActivity.java
+++ app/src/main/java/com/example/myeventsapp/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.myeventsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String LOG_TAG = "context(MainActivity)";
+
+    Button continueWithGoogleBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        continueWithGoogleBtn = findViewById(R.id.continue_with_google_btn);
+        continueWithGoogleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(LOG_TAG, "Continue with Google (yet to implement)");
+            }
+        });
     }
 }
