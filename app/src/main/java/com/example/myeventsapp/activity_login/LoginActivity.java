b84743be87fdaf45212d2fc5466c33c049956365
Rahul Badgujar <44890430+imRB7here@users.noreply.github.com>
2021-01-08 20:32:20 +0530

    updated google-services.json

--- app/src/main/java/com/example/myeventsapp/activity_login/LoginActivity.java
+++ app/src/main/java/com/example/myeventsapp/activity_login/LoginActivity.java
@@ -85,7 +85,7 @@ public class LoginActivity extends AppCompatActivity {
     private void createRequest() {
         // build Default GoogleSignInOption
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.oAuth_client_id))    // providing oAuth Client ID
+                .requestIdToken(getString(R.string.default_web_client_id))    // providing oAuth Client ID
                 .requestEmail() // requesting Email to be selected
                 .build();
         // Build a GoogleSignInClient with the options specified by gso.
